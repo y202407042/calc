dcef32cc9b643b85b1adb4e53b8068ca36bfc824
y202407042 <minseo583015@gmail.com>
2025-06-07 14:44:20 +0900

    add 기능 구현: add.cpp, add.h 추가

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -1,6 +1,21 @@
 ﻿#include <iostream>
+#include "add.h"
+#include "sub.h"
+#include "dive.h"
+#include "mod.h"
+#include "mul.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x = 0;
+    int y = 0;
+
+    std::cout << "첫번째 수를 입력하세요 : ";
+    std::cin >> x;
+    std::cout << "두번째 수를 입력하세요 : ";
+    std::cin >> y;
+
+    printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
+    printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n",
+        add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
 }
